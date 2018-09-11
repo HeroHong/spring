7a3dba2ee057be0dfdeb51e36c6f2bb55dadad5f
jiyuhong <ji.yuhong@xinheyun.com>
2018-09-11 20:30:31 +0800

    '整合mybatis'

--- spring-boot/src/main/java/com/altman/config/MyBatisConfig.java
+++ spring-boot/src/main/java/com/altman/config/MyBatisConfig.java
@@ -1,6 +1,8 @@
 package com.altman.config;
 
 import lombok.Data;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
--- spring-boot/src/main/java/com/altman/user/repository/mybatisMapper/UserMapper.java
+++ spring-boot/src/main/java/com/altman/user/repository/mybatisMapper/UserMapper.java
@@ -2,9 +2,7 @@ package com.altman.user.repository.mybatisMapper;
 
 import com.altman.user.domain.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
 
-@Component
 @Mapper
 public interface UserMapper {
 
